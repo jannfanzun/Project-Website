c4620b89f3b381ee5f96cf48ee3037193c639c6a
Noah Benz <noah.benz@edu.tbz.ch>
2023-03-31 15:39:11 +0200

    feat: Added theme changing icons

--- darkmoon.js
+++
@@ -1,6 +0,0 @@
-const moonIcon = document.getElementById("icon-moon");
-const moonImg = moonIcon.querySelector("moon.png");
-
-moonIcon.addEventListener("click", function() {
-  moonIcon.src = "images/sun.png";
-});
