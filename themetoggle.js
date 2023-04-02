e667806fc4f0586b5c63b45668f55d33ddc5e6ed
Noah Benz <noah.benz@edu.tbz.ch>
2023-04-02 17:27:40 +0200

    wip: Adding different color text on theme-change

--- themetoggle.js
+++ themetoggle.js
@@ -1,29 +1,47 @@
-const moonIcon = document.getElementById("icon");
-const moonImg = moonIcon.querySelector("moon.png");
-
-if (localStorage.getItem('theme')) {
-    const theme = localStorage.getItem('theme')
+function updateTheme(theme) {
     document.body.className = theme;
-    
-    if (theme == "light") {
-        moonIcon.src = "images/moon.png";
-    } else {
-        moonIcon.src = "images/sun.png";
+    const infotexts = document.querySelectorAll("#info-text");
+    infotexts.forEach((infotext) => {
+      if (theme === "light") {
+        infotext.classList.remove("blackmode-text");
+      } else if (theme === "dark") {
+        infotext.classList.add("blackmode-text");
+      }
+    });
+  
+    const topOffers = document.querySelectorAll("#top-angebote");
+    topOffers.forEach((offer) => {
+      if (theme === "light") {
+        offer.classList.remove("blackmode-text");
+      } else if (theme === "dark") {
+        offer.classList.add("blackmode-text");
+      }
+    });
+  }
+  
+  const moonIcon = document.getElementById("icon");
+  
+  if (localStorage.getItem("theme")) {
+    const theme = localStorage.getItem("theme");
+    updateTheme(theme);
+    if (theme === "light") {
+      moonIcon.src = "images/moon.png";
+    } else if (theme === "dark") {
+      moonIcon.src = "images/sun.png";
     }
-}
-
-moonIcon.addEventListener("click", function() {
-
-    const theme = localStorage.getItem('theme')
-
+  }
+  
+  moonIcon.addEventListener("click", function () {
+    const theme = localStorage.getItem("theme");
+  
     if (theme === "dark") {
-        moonIcon.src = "images/moon.png";
-        localStorage.setItem('theme', 'light');
-        document.body.className = "light";
-
-    }else {
-        moonIcon.src = "images/sun.png";
-        localStorage.setItem('theme', 'dark');
-        document.body.className = "dark";
+      moonIcon.src = "images/moon.png";
+      localStorage.setItem("theme", "light");
+      updateTheme("light");
+    } else {
+      moonIcon.src = "images/sun.png";
+      localStorage.setItem("theme", "dark");
+      updateTheme("dark");
     }
-});
+  });
+  
