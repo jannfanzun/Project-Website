40ad80c2805ebfc49d5e1c7f392c9eef6a5ccc72
Noah Benz <noah.benz@hartmann.info>
2023-05-04 09:14:35 +0200

    feat: Added angebote und rezensionen

--- rezensionen.js
+++ rezensionen.js
@@ -17,7 +17,7 @@ const testimonials = [
     name: "Fabio Murari",
     position: "Mitarbeiter EKS",
     photo: "images/murari.jpg",
-    text: ""
+    text: "Ich bin beeindruckt von der Wirksamkeit des Trainings- und Ernährungsplans, den das Team bereitgestellt hat. Ich hatte anfangs meine Zweifel, aber nachdem ich ihn ausprobiert habe, kann ich sagen, dass es wirklich so einfach funktioniert. Die Professionalität und das Fachwissen des Teams haben mir sehr geholfen. Der Trainingsplan war gut strukturiert und effektiv, während der Ernährungsplan mir geholfen hat, meine Ziele zu erreichen und gleichzeitig meine Gesundheit zu verbessern. Ich kann diesen Service nur wärmstens empfehlen, da es sich wirklich lohnt, Zeit und Energie in die eigene Gesundheit und Fitness zu investieren."
   }
 
 ];
