28f652a712cfa4e62a8afc01a20f1b52793707df
Noah Benz <noah.benz@hartmann.info>
2023-05-05 10:25:39 +0200

    feat: Adding alert messages

--- app.js
+++ app.js
@@ -20,3 +20,38 @@ form.addEventListener("submit", (event) => {
         alert("Failed");
     });
 });
+
+
+
+
+
+const contactform = document.querySelector("form[name='newsletter-form']");
+const alertElement = document.querySelector("#message_alert");
+const success_message = document.getElementById("success-message");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // prevent form submission
+  fetch(event.target.action, {
+    method: "POST",
+    body: new FormData(event.target),
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      // show success message
+      alertElement.classList.remove("hidden");
+      alertElement.classList.remove("text-alert-red", "border-alert-red");
+      alertElement.classList.add("text-alert-green", "border-alert-green");
+      success_message.innerText = "Deine Nachricht wurde erfolgreich gesendet!";
+      // reset the form
+      contactform.reset();
+    })
+    .catch((error) => {
+      alertElement.classList.remove("hidden");
+      alertElement.classList.remove("text-alert-green", "border-alert-green");
+      alertElement.classList.add("text-alert-red", "border-alert-red");
+      success_message.innerText =
+        "Hoppla, da ist etwas schiefgelaufen, bitte versuche es erneut.";
+    });
+});
