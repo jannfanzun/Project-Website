bec3e5511d17e7aa54ea2a83436b06221fdfec3d
Noah Benz <noah.benz@hartmann.info>
2023-05-05 14:58:35 +0200

    fix: Changed testimonial text and changed color

--- rezensionen.js
+++ rezensionen.js
@@ -11,13 +11,13 @@ const testimonials = [
     photo:
       "images/eliasbrasser.jpeg",
     text:
-    "Seitdem ich mich MDC angeschlossen habe, konnte ich eine kontinuierliche Zunahme meiner Muskelmasse feststellen. Ich bin erstaunt darüber, wie reibungslos das gesamte Trainingsprogramm abläuft und wie gut alles organisiert ist. Ich bereue es keineswegs, mich für MDC entschieden zu haben und bin dankbar für die Unterstützung auf meinem Fitnessweg."
+    "Bei MDC habe ich kontinuierliche Muskelzunahme erlebt. Das Trainingsprogramm ist reibungslos und gut organisiert. Ich bereue meine Entscheidung für MDC nicht und bin dankbar für die Unterstützung auf meinem Fitnessweg."
   },
   {
     name: "Fabio Murari",
     position: "Mitarbeiter EKS",
     photo: "images/murari.jpg",
-    text: "Ich bin beeindruckt von der Wirksamkeit des Trainings- und Ernährungsplans, den das Team bereitgestellt hat. Ich hatte anfangs meine Zweifel, aber nachdem ich ihn ausprobiert habe, kann ich sagen, dass es wirklich so einfach funktioniert. Die Professionalität und das Fachwissen des Teams haben mir sehr geholfen. Der Trainingsplan war gut strukturiert und effektiv, während der Ernährungsplan mir geholfen hat, meine Ziele zu erreichen und gleichzeitig meine Gesundheit zu verbessern. Ich kann diesen Service nur wärmstens empfehlen, da es sich wirklich lohnt, Zeit und Energie in die eigene Gesundheit und Fitness zu investieren."
+    text: "Der Trainings- und Ernährungsplan hat funktioniert und meine Gesundheit verbessert. Das professionelle Team und die effektiven Pläne halfen mir, meine Ziele zu erreichen. Ich empfehle diesen Service sehr."
   }
 
 ];
