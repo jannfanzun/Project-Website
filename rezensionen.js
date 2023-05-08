a0b2615c33fb27bacdc1877082332316aabd76be
jannfanzun <110892321+jannfanzun@users.noreply.github.com>
2023-05-08 11:08:57 +0200

    wip: testomonials

--- rezensionen.js
+++ rezensionen.js
@@ -18,7 +18,20 @@ const testimonials = [
     position: "Mitarbeiter EKS",
     photo: "images/murari.jpg",
     text: "Der Trainings- und Ernährungsplan hat funktioniert und meine Gesundheit verbessert. Das professionelle Team und die effektiven Pläne halfen mir, meine Ziele zu erreichen. Ich empfehle diesen Service sehr."
+  },
+  {
+    name: "",
+    position:"",
+    photo: "images/midfrau.jpg",
+    text: "Ich denke, dieser Ernährungsplan war eine gute Wahl für mich. Es hat mich auf den richtigen Weg gebracht und Fortschritte gemacht. Ich werde es auf jeden Fall weiterhin nutzen und empfehle es jedem, der nach einem einfachen und effektiven Ernährungsplan sucht."
+  },
+  {
+    name: "Roberto Kreienbühl",
+    position: "Lehrling Toller AG",
+    photo: "images/robrerto.jpg",
+    text: "Dank des großartigen Trainingsplans meines Fitness-Teams habe ich in 5 Monaten viele Fortschritte gemacht. Ich bin sehr zufrieden und kann es jedem empfehlen, der nach einem maßgeschneiderten und effektiven Plan sucht. Vielen Dank MDCoaching!"
   }
+    
 
 ];
 
