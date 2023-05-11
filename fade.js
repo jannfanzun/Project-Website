8280d0e21dffdd44aa77f7856ffe25e1ed646834
jannfanzun <110892321+jannfanzun@users.noreply.github.com>
2023-05-11 12:08:25 +0200

    feat: animation on scroll

--- fade.js
+++ fade.js
@@ -1,26 +1,27 @@
- // Animationsfunktion für das Scrollen
- function animateOnScroll() {
+// Animationsfunktion für das Scrollen
+function animateOnScroll() {
     const elements = document.querySelectorAll('.animate-on-scroll');
-
+  
     elements.forEach((element) => {
-      if (isElementInViewport(element)) {
+      if (!element.classList.contains('animated') && isElementInViewport(element)) {
         element.classList.add('animated');
       }
     });
   }
-
+  
   // Funktion zum Überprüfen, ob ein Element im sichtbaren Bereich des Viewports liegt
   function isElementInViewport(element) {
     const rect = element.getBoundingClientRect();
     return (
       rect.top >= 0 &&
       rect.left >= 0 &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
       rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
   }
-
-  // Überprüfen der Elemente beim Scrollen
+  
+  // Überprüfen der Elemente beim Scrollen und beim Ändern der Viewport-Größe
   window.addEventListener('scroll', animateOnScroll);
+  window.addEventListener('resize', animateOnScroll);
   animateOnScroll(); // Führe die Animation beim Laden der Seite aus
+  
