895f36c3f5e45e18eaad670a5e165ed3a357105a
Noah Benz <noah.benz@edu.tbz.ch>
2023-05-13 13:36:10 +0200

    wip: Fixing contact and newsletter form

--- app.js
+++ app.js
@@ -1,32 +1,4 @@
 const form = document.querySelector("form[name='newsletter-form']");
-
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault(); // prevent form submission
-  fetch(event.target.action, {
-    method: "POST",
-    body: new FormData(event.target),
-    headers: {
-      Accept: "application/json",
-    },
-  })
-
-    .then((response) => {
-      form.reset();
-      
-
-    })
-    .catch((error) => {
-       
-    });
-});
-
-
-
-
-// Newsletter Form
-
-const contactform = document.querySelector("form[name='newsletter-form']");
 const alertElement = document.querySelector("#response-message-box");
 const success_message = document.getElementById("alert-message");
 
@@ -42,11 +14,11 @@ form.addEventListener("submit", (event) => {
     .then((response) => {
       // show success message
       alertElement.classList.remove("hidden");
-      alertElement.classList.remove("text-alert-red", "border-alert-red");
+      alertElement.classList.remove("hidden");
       alertElement.classList.add("text-alert-green", "border-alert-green");
-      success_message.innerText = "Du wurdest erfolgreich registriert!";
+      success_message.innerText = "Deine Nachricht wurde erfolgreich gesendet!";
       // reset the form
-      contactform.reset();
+      form.reset();
     })
     .catch((error) => {
       alertElement.classList.remove("hidden");
@@ -56,39 +28,4 @@ form.addEventListener("submit", (event) => {
         "Hoppla, da ist etwas schiefgelaufen, bitte versuche es erneut.";
     });
 });
-
-
-
-
-
-// Contact Form
-
-const alertContainer = document.querySelector("#alertContainer");
-const alertMessage = document.getElementById("alertMessage");
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault(); // prevent form submission
-  fetch(event.target.action, {
-    method: "POST",
-    body: new FormData(event.target),
-    headers: {
-      Accept: "application/json",
-    },
-  })
-    .then((response) => {
-      // show success message
-      alertContainer.classList.remove("hidden");
-      alertContainer.classList.remove("text-alert-red", "border-alert-red");
-      alertContainer.classList.add("text-alert-green", "border-alert-green");
-      alertMessage.innerText = "Deine Nachricht wurde erfolgreich gesendet!";
-      // reset the form
-      contactform.reset();
-    })
-    .catch((error) => {
-      alertContainer.classList.remove("hidden");
-      alertContainer.classList.remove("text-alert-green", "border-alert-green");
-      alertContainer.classList.add("text-alert-red", "border-alert-red");
-      alertMessage.innerText =
-        "Hoppla, da ist etwas schiefgelaufen, bitte versuche es erneut.";
-    });
-});
+onsubmit="clear_form()"
